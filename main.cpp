37f6e6ac2c525b8d755be3a9748b8b7d37c09cfb
Will Lane <williamlane923@gmail.com>
2021-04-22 17:16:14 -0700

    add base files

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <cstdio>
+#include <iostream>
+
+static void error_callback(int error, const char* description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (action == GLFW_PRESS) {
+    std::cout << (char) key;
+    fflush(stdout);
+  }
+
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+  GLFWwindow* window;
+
+  glfwSetErrorCallback(error_callback);
+
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  window = glfwCreateWindow(640, 480, "Simple example", nullptr, nullptr);
+  if (!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+
+  glfwMakeContextCurrent(window);
+  gladLoadGL(glfwGetProcAddress);
+  glfwSwapInterval(1);
+
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
+}
